74a45f3384583258963180b434bf47e6888eeb2c
Lijauco, Carlo Cedric <carlocedric.lijauco@bertelsmann.de>
2016-09-07 11:35:38 +0800

    Initial Commit

---
+++ ClaimsProject/ClaimsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClaimsProject
+//
+//  Created by Lijauco, Carlo Cedric on 07/09/2016.
+//  Copyright © 2016 arvato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
